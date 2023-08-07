cafac5fab17addec5b5a71da4951b2c919b1a7e2
Vitor Medeiros <lennon_vms@hotmail.com>
2023-08-07 15:13:30 -0300

    Add project files.

---
+++ Exercicios_Fixacao/Exercicios_Fixacao.cpp
@@ -0,0 +1,13 @@
+// Exercicios_Fixacao.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
