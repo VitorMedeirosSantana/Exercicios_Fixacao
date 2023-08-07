0352d8f3faf0a1bf9880c2410ef75fbf3b7d3dff
Vitor Medeiros <lennon_vms@hotmail.com>
2023-08-07 18:13:47 -0300

    Atualizado

--- Palindromo_inteiro/teste.cpp
+++ Palindromo_inteiro/teste.cpp
@@ -1,20 +1,37 @@
-// Palindromo_inteiro.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+bool isPalindrome(int);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int test1 = 123454321;
+	int test2 = 123454545321;
+
+	if (isPalindrome(test1))
+		cout << "This number is Palindrome";
+	else
+		cout << "This number isn't Palindrome";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool isPalindrome(int x)
+{
+	if (x < 0 || (x % 10 == 0 && x != 0))
+	{
+		return false;
+	}
+
+	if (!x)
+		return true;
+
+	int inversao{};
+	while (x > inversao)
+	{
+		inversao = inversao * 10 + x % 10;
+		x /= 10;
+	}
+	return x == inversao || x == inversao / 10;
+
+}
